985749f0cb55ed08f5bf6b48852e62dcf8ee7d43
Lawhool <nextraile@gmail.com>
2026-07-20 12:45:35 +0700

    Refactor TCP server logging for clarity and initialization (#15)
    * refactor(tcp-server): improve logging clarity
    
    * refactor(index): initialize TCP server and added startup logging
    
    * refactor(tcp-server): update logging for received data in handleSocketData function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 process.loadEnvFile('.env');
 import appConfig from './config/environment.js';
+import * as tcpServer from './network/tcp-server.js';
+
+console.log("(index): Starting application...");
+tcpServer.startServer(appConfig.networks.tcp.port);
