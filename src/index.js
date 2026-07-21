eb382f509e1b8719d7e4e787c710fef8af9c2876
Nextraile <nextraile@gmail.com>
2026-07-21 08:27:16 +0700

    refactor(tcp-server): initialize TCP server instance and handle data streams

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import appConfig from './config/environment.js';
 import * as tcpServer from './network/tcp-server.js';
 
 console.log("(index): Starting application...");
-tcpServer.startServer(appConfig.networks.tcp.port);
+const tcpServerInstance = tcpServer.startServer(appConfig.networks.tcp.port);
+
+tcpServer.handleStreamError(tcpServerInstance, (err) => {
+  console.error(`(index): Error occurred: ${err.message}`);
+});
+
+tcpServer.handleDataStream(tcpServerInstance, (data) => {
+  console.log(`(index): Received data: ${data.toString('utf-8')}`);
+});
